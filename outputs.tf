1c5daa97df8880aeb94bf66e52a363b876ab4fc7
jonnywillmac <jonathan@ssmj.co.uk>
2019-08-29 17:29:23 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
-output "Zone1-VPN-Peer" {
-  value = "${ibm_is_vpn_gateway.VPNGateway1.public_ip_address}"
-}
-
-output "Zone2-VPN-Peer" {
-  value = "${ibm_is_vpn_gateway.VPNGateway2.public_ip_address}"
-}
-
 output "master_db" {
   value = "${ibm_is_instance.dbserver-zone1.0.primary_network_interface.0.primary_ipv4_address}"
 }
